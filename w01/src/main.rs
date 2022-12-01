adc8a949cd94a2b932c150fd7a1ae3644a429bbd
Dominik Gresch <mail@greschd.ch>
2022-12-01 01:29:47 +0100

    Rust book chapter 2 guessing game

--- w01/src/main.rs
+++ w01/src/main.rs
@@ -1,3 +1,27 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..=20);
+
+    loop {
+        println!("Guess the number: ");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("faill!!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{} is not a number", guess.trim());
+                continue;
+            }
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
